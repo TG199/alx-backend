e3acdb7c3887d705b91ff6299f29ee331e60810c
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-10 20:28:20 +0100

    simple blog app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def home():
+def index():
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
