27280900cab37a3aa965253f3d1242d03e03fd6d
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-10 20:29:54 +0100

    home route

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
+    """
+    Home route
+    """
     return render_template('0-index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
