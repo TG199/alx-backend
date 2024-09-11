ea7e1c7bf1d2c5f5e3b40992ddb96fb2d40ebca6
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-11 05:12:07 +0100

    translations

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Simple route modle
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale function"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    Home route
+    """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
