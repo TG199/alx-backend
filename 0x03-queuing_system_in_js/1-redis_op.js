0ef2254ce82d7933d7248c12120217ffb0dcbd55
TG <ebiritg@gmail.com>
2024-10-10 00:41:59 +0100

    set and get

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+import redis from "redis";
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+client.on('error', () => {
+    console.console.log('Redis client not connected to the server: ERROR_MESSAGE');
+});
+
+const setNewSchool = (schoolName, value) => {
+    client.set(schoolName, value);
+    redis.print(`Successfully set the value ${schoolName}`);
+}
+
+const displaySchoolValue = (schoolName) => {
+    client.get(schoolName, (err, reply) => {
+        if (err) {
+          console.error(`Error retrieving value: ${err.message}`);
+        } else {
+          console.log(reply);
+        }
+      });
+}
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
